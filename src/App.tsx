bf38fd2eb407e21239e481f928bb8caf3b495d4c
VadimLNN <svobodolubsev@inbox.ru>
2024-06-10 20:46:06 +0700

    удаление ненужного и написание микроформочки

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { SubmitHandler, useForm } from "react-hook-form";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface MyForm {
+    name: string;
+    age: number;
 }
 
-export default App
+function App() {
+    const { register, handleSubmit } = useForm<MyForm>({
+        defaultValues: {
+            age: 18,
+        },
+    });
+
+    const submit: SubmitHandler<MyForm> = (data) => {
+        console.log(data);
+    };
+
+    return (
+        <>
+            <form onSubmit={handleSubmit(submit)}>
+                <input type="text" {...register("name", { required: true })}></input>
+                <input type="number" {...register("age", { required: true })}></input>
+                <button>Send</button>
+            </form>
+        </>
+    );
+}
+
+export default App;
