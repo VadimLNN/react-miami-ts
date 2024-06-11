2c79d17dfb4b368b74fe18eecfd73c5b494736c1
VadimLNN <svobodolubsev@inbox.ru>
2024-06-11 14:22:19 +0700

    страничка для кастомных цветов

---
+++ src/components/ColorTokens/ColorTokens.tsx
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material/styles";
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#E0514F",
+        },
+        secondary: {
+            main: "#398BE3",
+            light: "#C5D7FF",
+            contrastText: "#ffffff",
+        },
+    },
+});
