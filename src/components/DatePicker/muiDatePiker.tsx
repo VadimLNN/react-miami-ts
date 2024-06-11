32c9bfba28dd991e15f05c9a1a199bbfda7b7f51
VadimLNN <svobodolubsev@inbox.ru>
2024-06-11 20:51:29 +0700

    добавление компонента в redux toolkit

--- src/components/DatePicker/muiDatePiker.tsx
+++ src/components/DatePicker/muiDatePiker.tsx
@@ -3,12 +3,21 @@ import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { useAppDispatch } from "../../hook";
+import { handleChangeDate } from "../../store/formSlice";
 
 export default function DatePickerValue() {
+    const dispatch = useAppDispatch();
+
+    const changeDate = (x: any) => {
+        let cityName = JSON.stringify(x).substring(1, 11);
+        dispatch(handleChangeDate(cityName));
+    };
+
     return (
         <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <DemoContainer components={["DatePicker", "DatePicker"]}>
-                <DatePicker label="Дата рождения" defaultValue={dayjs("2024-06-11")} />
+            <DemoContainer components={["DatePicker"]}>
+                <DatePicker label="Дата рождения" defaultValue={dayjs("2024-06-11")} onChange={(x) => changeDate(x)} />
             </DemoContainer>
         </LocalizationProvider>
     );
