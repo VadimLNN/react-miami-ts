4f2fec566e42e46806927c448f49515cf85aa436
VadimLNN <svobodolubsev@inbox.ru>
2024-06-11 20:52:26 +0700

    добавление компонента для выбора города в redux toolkit

--- src/components/Select/muiSelect.tsx
+++ src/components/Select/muiSelect.tsx
@@ -1,25 +1,24 @@
-import * as React from "react";
 import Box from "@mui/material/Box";
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
+import { useAppDispatch, useAppSelector } from "../../hook";
+import { handleChangeSelect } from "../../store/formSlice";
 
 export default function BasicSelect() {
-    const [city, setCity] = React.useState("");
-
-    const handleChange = (event: SelectChangeEvent) => {
-        setCity(event.target.value);
-    };
+    const changeCityName = (event: SelectChangeEvent) => dispatch(handleChangeSelect(event.target.value));
+    const dispatch = useAppDispatch();
+    const city = useAppSelector((state) => state.formReduser.cityName);
 
     return (
         <Box sx={{ minWidth: 120 }}>
             <FormControl fullWidth>
                 <InputLabel id="demo-simple-select-label">Город</InputLabel>
-                <Select labelId="demo-simple-select-label" id="demo-simple-select" value={city} label="Город" onChange={handleChange}>
-                    <MenuItem value={"г. Новосибирск"}>Новосибирск</MenuItem>
-                    <MenuItem value={"г. Барнаул"}>Барнаул</MenuItem>
-                    <MenuItem value={"г. Бердск"}>Бердск</MenuItem>
+                <Select labelId="demo-simple-select-label" id="demo-simple-select" value={city} label="Город" onChange={changeCityName}>
+                    <MenuItem value={"г. Новосибирск"}>г. Новосибирск</MenuItem>
+                    <MenuItem value={"г. Барнаул"}>г. Барнаул</MenuItem>
+                    <MenuItem value={"г. Бердск"}>г. Бердск</MenuItem>
                 </Select>
             </FormControl>
         </Box>
