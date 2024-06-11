d4d0efa4cfe3554eebad8112faa67835f620c3b8
VadimLNN <svobodolubsev@inbox.ru>
2024-06-11 20:53:01 +0700

    мега прокачка страницы регистрации

--- src/pages/registerForm.tsx
+++ src/pages/registerForm.tsx
@@ -1,77 +1,107 @@
-import { TextField, Button, Stack, RadioGroup, FormControlLabel, Radio, FormControl, FormLabel, Typography } from "@mui/material";
+import { TextField, Button, Stack, FormControlLabel, Typography, Grid, Paper, Checkbox } from "@mui/material";
 import { useForm } from "react-hook-form";
 import { DevTool } from "@hookform/devtools";
 import DatePickerValue from "../components/DatePicker/muiDatePiker";
 import BasicSelect from "../components/Select/muiSelect";
+import RadioGroupGender from "../components/RadioGroup/radioGroup";
+import { useAppSelector } from "../hook";
 
 type FormValues = {
+    fio: string;
+    date_of_birth: string;
+    gender: string;
+    city: string;
     email: string;
     password: string;
-    fio: string;
+    agree: boolean;
 };
 
-export const MuiLoginForm = () => {
+export const MuiRegisterForm = () => {
     const form = useForm<FormValues>({
         defaultValues: {
+            fio: "",
+            date_of_birth: "",
+            gender: "",
+            city: "",
             email: "",
             password: "",
+            agree: false,
         },
     });
 
     const { register, handleSubmit, formState, control } = form;
-
     const { errors } = formState;
-    //!!!!
+
+    const date = useAppSelector((state) => state.formReduser.date);
+    const city = useAppSelector((state) => state.formReduser.cityName);
+    const gendr = useAppSelector((state) => state.formReduser.gender);
+
     const onSubmit = (data: FormValues) => {
+        data.date_of_birth = date;
+        data.city = city;
+        data.gender = gendr;
+
         console.log(data);
     };
 
     return (
-        <div>
-            <Typography variant="h4">Регистрация</Typography>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <Stack spacing={2} width={400}>
-                    <TextField
-                        label="ФИО"
-                        type="fio"
-                        {...register("fio", { required: "FIO is requied" })}
-                        error={!!errors.password}
-                        helperText={errors.password?.message}
-                    />
+        <Paper
+            sx={{
+                p: 2,
+                margin: "auto",
+                maxWidth: 500,
+                flexGrow: 1,
+                backgroundColor: (theme) => (theme.palette.mode === "dark" ? "#1A2027" : "#fff"),
+            }}
+        >
+            <Grid>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <Stack spacing={2} width={500}>
+                        <Typography variant="h4">Регистрация</Typography>
 
-                    <DatePickerValue />
+                        <TextField
+                            label="ФИО"
+                            type="fio"
+                            {...register("fio", { required: "FIO is requied" })}
+                            error={!!errors.password}
+                            helperText={errors.password?.message}
+                        />
 
-                    <FormControl>
-                        <FormLabel id="demo-radio-buttons-group-label">Пол</FormLabel>
-                        <RadioGroup row aria-labelledby="demo-row-radio-buttons-group-label" name="row-radio-buttons-group">
-                            <FormControlLabel value="female" control={<Radio />} label="Ж" />
-                            <FormControlLabel value="male" control={<Radio />} label="М" />
-                        </RadioGroup>
-                    </FormControl>
+                        <DatePickerValue />
 
-                    <BasicSelect />
+                        <RadioGroupGender />
 
-                    <TextField
-                        label="Почта"
-                        type="email"
-                        {...register("email", { required: "Email is requied" })}
-                        error={!!errors.email}
-                        helperText={errors.email?.message}
-                    />
-                    <TextField
-                        label="ПОРОЛЬ"
-                        type="password"
-                        {...register("password", { required: "Password is requied" })}
-                        error={!!errors.password}
-                        helperText={errors.password?.message}
-                    />
+                        <BasicSelect />
 
-                    <Button type="submit" variant="contained" color="primary">
-                        Login
-                    </Button>
-                </Stack>
-            </form>
-            <DevTool control={control} />
-        </div>
+                        <TextField
+                            label="Почта"
+                            type="email"
+                            {...register("email", { required: "Email is requied" })}
+                            error={!!errors.email}
+                            helperText={errors.email?.message}
+                        />
+                        <TextField
+                            label="ПОРОЛЬ"
+                            type="password"
+                            {...register("password", { required: "Password is requied" })}
+                            error={!!errors.password}
+                            helperText={errors.password?.message}
+                        />
+
+                        <FormControlLabel
+                            value={true}
+                            control={<Checkbox />}
+                            label="Я принимаю условия Лицензионного соглашения, Политики конфиденциальности и даю согласие на обработку данных обо мне."
+                            labelPlacement="end"
+                        />
+
+                        <Button type="submit" variant="contained" color="primary">
+                            Login
+                        </Button>
+                    </Stack>
+                </form>
+                <DevTool control={control} />
+            </Grid>
+        </Paper>
     );
 };
