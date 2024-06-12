6629bfce25d2f531588c927dd1834f96372b2564
VadimLNN <svobodolubsev@inbox.ru>
2024-06-12 17:15:30 +0700

    отдельные компоненты для формы

---
+++ src/components/form-components/FormInputDate.tsx
@@ -0,0 +1,13 @@
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFnsV3";
+import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { Controller } from "react-hook-form";
+import { FormInputProps } from "./FormInputProps";
+
+export const FormInputDate = ({ name, control, label }: FormInputProps) => {
+    return (
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+            <Controller name={name} control={control} render={({ field: { onChange, value } }) => <DatePicker value={value} onChange={onChange} />} />
+        </LocalizationProvider>
+    );
+};
--- src/pages/myForm.tsx
+++ src/pages/myForm.tsx
@@ -1,41 +1,65 @@
-import { Controller, SubmitHandler, useForm } from "react-hook-form";
-interface MyForm {
-    name: string;
-    age: number;
+import { Button, Paper, Typography } from "@mui/material";
+import { useForm } from "react-hook-form";
+import { FormInputText } from "../components/form-components/FormInputText";
+import { FormInputMultiCheckbox } from "../components/form-components/FormInputMultiCheckbox";
+import { FormInputDropdown } from "../components/form-components/FormInputDropdown";
+import { FormInputDate } from "../components/form-components/FormInputDate";
+import { FormInputRadio } from "../components/form-components/FormInputRadio";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+interface IFormInput {
+    textValue: string;
+    radioValue: string;
+    // checkboxValue: string[];
+    // dateValue: Date;
+    // dropdownValue: string;
 }
-const RegisterFormPage = () => {
-    const { register, handleSubmit, reset, watch, control } = useForm<MyForm>({
-        defaultValues: {
-            age: 18,
-        },
-    });
 
-    const submit: SubmitHandler<MyForm> = (data) => {
-        console.log(data);
-    };
-
-    return (
-        <>
-            <form onSubmit={handleSubmit(submit)}>
-                <input type="text" {...register("name", { required: true })}></input>
-                <Controller name="age" control={control} render={({ field }) => <input {...field} />} />
-
-                <button>Send</button>
-                <button
-                    type="button"
-                    onClick={() =>
-                        reset({
-                            age: 0,
-                            name: "",
-                        })
-                    }
-                >
-                    Reset fields
-                </button>
-            </form>
-            {watch("age")}
-        </>
-    );
+const defaultValues = {
+    textValue: "",
+    radioValue: "",
+    // checkboxValue: [],
+    // dateValue: new Date(),
+    // dropdownValue: "",
 };
 
-export default RegisterFormPage;
+const validationSchema = yup.object({
+    textValue: yup.string().required("Full Name is required"),
+    radioValue: yup.string().required("Full Name is required"),
+});
+
+export const FormDemo = () => {
+    const { handleSubmit, reset, control, setValue } = useForm<IFormInput>({
+        defaultValues: defaultValues,
+        resolver: yupResolver(validationSchema),
+    });
+
+    const onSubmit = (data: IFormInput) => console.log(data);
+
+    return (
+        <Paper
+            style={{
+                display: "grid",
+                gridRowGap: "20px",
+                padding: "20px",
+                margin: "10px 300px",
+            }}
+        >
+            <Typography variant="h4"> Form Demo</Typography>
+
+            <FormInputText name="textValue" control={control} label="Text Input" />
+            <FormInputRadio name={"radioValue"} control={control} label={"Radio Input"} />
+            <FormInputDropdown name="dropdownValue" control={control} label="Dropdown Input" />
+            <FormInputDate name="dateValue" control={control} label="Date Input" />
+            <FormInputMultiCheckbox control={control} setValue={setValue} name={"checkboxValue"} label={"Checkbox Input"} />
+
+            <Button onClick={handleSubmit(onSubmit)} variant={"contained"}>
+                Submit
+            </Button>
+            <Button onClick={() => reset()} variant={"outlined"}>
+                Reset
+            </Button>
+        </Paper>
+    );
+};
