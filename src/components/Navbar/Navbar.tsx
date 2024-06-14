5252f847f21d59bed5f0bf6c7de6b5bc4c636b90
VadimLNN <svobodolubsev@inbox.ru>
2024-06-14 18:33:24 +0700

    переписал навбар

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,9 +1,10 @@
 import { AppBar, Toolbar, IconButton, Typography, Stack, Button } from "@mui/material";
 import CatchingPokemonIcon from "@mui/icons-material/CatchingPokemon";
+import { NavLink } from "react-router-dom";
 
 export default function Navbar() {
     return (
-        <AppBar className="nav" position="static">
+        <AppBar>
             <Toolbar>
                 <IconButton size="large" edge="start" color="inherit" aria-label="logo">
                     <CatchingPokemonIcon />
@@ -12,9 +13,15 @@ export default function Navbar() {
                     Miami
                 </Typography>
                 <Stack direction="row" spacing={2}>
-                    <Button color="inherit">Main</Button>
-                    <Button color="inherit">Form</Button>
-                    <Button color="inherit">Some thing</Button>
+                    <NavLink to="/mainForm">
+                        <Button color="inherit">MainForm</Button>
+                    </NavLink>
+                    <NavLink to="/secondForm">
+                        <Button color="inherit">SecondForm</Button>
+                    </NavLink>
+                    <NavLink to="/">
+                        <Button color="inherit">Some thing</Button>
+                    </NavLink>
                 </Stack>
             </Toolbar>
         </AppBar>
