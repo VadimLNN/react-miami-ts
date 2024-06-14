7b798f9226d7a61f37ba0afa4a4b5bbfef67eb0f
VadimLNN <svobodolubsev@inbox.ru>
2024-06-14 18:34:35 +0700

    добавление параметров из темы

--- src/pages/myForm.tsx
+++ src/pages/myForm.tsx
@@ -1,12 +1,14 @@
-import { Button, Paper, Typography } from "@mui/material";
+import { Button, Paper, ThemeProvider, Typography } from "@mui/material";
+import * as yup from "yup";
 import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
 import { FormInputText } from "../components/form-components/FormInputText";
 import { FormInputMultiCheckbox } from "../components/form-components/FormInputMultiCheckbox";
 import { FormInputDropdown } from "../components/form-components/FormInputDropdown";
 import { FormInputDate } from "../components/form-components/FormInputDate";
 import { FormInputRadio } from "../components/form-components/FormInputRadio";
-import * as yup from "yup";
-import { yupResolver } from "@hookform/resolvers/yup";
+
+import DemoTheme from "../components/DemoTheme/demoTheme";
 
 interface IFormInput {
     textValue: string;
@@ -38,28 +40,23 @@ export const FormDemo = () => {
     const onSubmit = (data: IFormInput) => console.log(data);
 
     return (
-        <Paper
-            style={{
-                display: "grid",
-                gridRowGap: "20px",
-                padding: "20px",
-                margin: "10px 300px",
-            }}
-        >
-            <Typography variant="h4"> Form Demo</Typography>
+        <ThemeProvider theme={DemoTheme()}>
+            <Paper>
+                <Typography variant="h4"> Form Demo</Typography>
 
-            <FormInputText name="textValue" control={control} label="Text Input" />
-            <FormInputRadio name={"radioValue"} control={control} label={"Radio Input"} />
-            <FormInputDropdown name="dropdownValue" control={control} label="Dropdown Input" />
-            <FormInputDate name="dateValue" control={control} label="Date Input" />
-            <FormInputMultiCheckbox control={control} setValue={setValue} name={"checkboxValue"} label={"Checkbox Input"} />
+                <FormInputText name="textValue" control={control} label="Text Input" />
+                <FormInputRadio name={"radioValue"} control={control} label={"Radio Input"} />
+                <FormInputDropdown name="dropdownValue" control={control} label="Dropdown Input" />
+                <FormInputDate name="dateValue" control={control} label="Date Input" />
+                <FormInputMultiCheckbox control={control} setValue={setValue} name={"checkboxValue"} label={"Checkbox Input"} />
 
-            <Button onClick={handleSubmit(onSubmit)} variant={"contained"}>
-                Submit
-            </Button>
-            <Button onClick={() => reset()} variant={"outlined"}>
-                Reset
-            </Button>
-        </Paper>
+                <Button onClick={handleSubmit(onSubmit)} variant={"contained"}>
+                    Submit
+                </Button>
+                <Button onClick={() => reset()} variant={"outlined"}>
+                    Reset
+                </Button>
+            </Paper>
+        </ThemeProvider>
     );
 };
