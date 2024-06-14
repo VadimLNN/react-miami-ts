ce66a783fae6efbd4304dc552c915723a6e51e4b
VadimLNN <svobodolubsev@inbox.ru>
2024-06-14 18:26:45 +0700

    написание параметров отдельным компонентам

--- src/components/DemoTheme/demoTheme.tsx
+++ src/components/DemoTheme/demoTheme.tsx
@@ -2,15 +2,53 @@ import { createTheme } from "@mui/material/styles";
 
 const DemoTheme = () => {
     return createTheme({
-        components: {},
+        components: {
+            MuiAppBar: {
+                styleOverrides: {
+                    root: {
+                        margin: "0px",
+                        padding: "0.5em",
+                        position: "static",
+                    },
+                },
+            },
+            MuiPaper: {
+                styleOverrides: {
+                    root: {
+                        display: "grid",
+                        // gridRowGap: "20px",
+                        // padding: "20px",
+                        // margin: "10px 300px",
+                        flexGrow: 1,
+                        width: 500,
+                        gridRowGap: "20px",
+                        padding: "2em",
+                        margin: "2em",
+                    },
+                },
+            },
+            // MuiTypography: {
+            //     styleOverrides: {
+            //         root: {
+            //             color: "inherit",
+            //         },
+            //     },
+            // },
+            MuiButton: {
+                styleOverrides: {
+                    root: {
+                        color: "white",
+                        size: "large",
+                    },
+                },
+            },
+        },
         palette: {
             primary: {
-                main: "#E0514F",
+                main: "#4C7EE0",
             },
             secondary: {
-                main: "#398BE3",
-                light: "#C5D7FF",
-                contrastText: "#ffffff",
+                main: "#E34E30",
             },
         },
     });
