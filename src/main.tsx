abe6c56015b1dff5b6afae84f91027692b5d79a5
VadimLNN <svobodolubsev@inbox.ru>
2024-06-14 18:36:38 +0700

    добавление стилей

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from "react";
+import { Provider } from "react-redux";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
 import store from "./store/index.tsx";
-import { Provider } from "react-redux";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
     <React.StrictMode>
