ca26074bbd1d56966011c47dfcd24b47f03bd38b
VadimLNN <svobodolubsev@inbox.ru>
2024-06-21 12:00:37 +0700

    отдельные конфиги под разные url

--- src/components/api/requests.tsx
+++ src/components/api/requests.tsx
@@ -9,13 +9,28 @@ axios.defaults.headers.common = {
 };
 axios.defaults.withCredentials = true;
 
+const firstApiAxios = axios.create({
+    baseURL: FIRST_API_URL,
+    headers: {
+        Authorization: `Bearer $sdfdfsghgfhvc}`,
+    },
+    withCredentials: false,
+});
+const secondApiAxios = axios.create({
+    baseURL: SECOND_API_URL,
+    headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+    },
+    withCredentials: true,
+});
+
 // promis style
 // export const getPosts = axios({ url: `${FIRST_API_URL}/posts`, method: "GET", params: { offset: 0 } });
 
 // async await style
 export const getPosts = async () => {
     try {
-        const res = await axios.get(`/posts`, {
+        const res = await firstApiAxios.get(`/posts`, {
             params: { offset: 0, limit: 10 },
         });
         console.log(res.data);
@@ -29,10 +44,22 @@ export const getPosts = async () => {
 
 export const createPost = async () => {
     try {
-        const res = await axios.post(`/posts`, {
+        const res = await firstApiAxios.post(`/posts`, {
             body: "world",
             title: "helo",
         });
+        console.log(res.data);
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            console.log(error, "err");
+            console.log(error.response?.data.errText, "error");
+        } else if (error instanceof Error) console.log(error.message);
+    }
+};
+
+export const getImg = async () => {
+    try {
+        const res = await secondApiAxios.get("/images");
         console.log(res.data);
     } catch (error) {
         if (axios.isAxiosError(error)) {
