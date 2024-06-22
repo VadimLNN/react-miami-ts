7a984804ef52efa6cc3901f367931d72cf33da4b
VadimLNN <svobodolubsev@inbox.ru>
2024-06-22 22:14:22 +0700

    удаление вынесенного кода

--- src/app/providers/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider.tsx
@@ -6,39 +6,7 @@ const DemoTheme = () => {
             MuiAppBar: {
                 styleOverrides: {
                     root: {
-                        margin: "0px",
-                        padding: "0.5em",
-                        position: "static",
-                    },
-                },
-            },
-            MuiPaper: {
-                styleOverrides: {
-                    root: {
-                        display: "grid",
-                        // gridRowGap: "20px",
-                        // padding: "20px",
-                        // margin: "10px 300px",
-                        flexGrow: 1,
-                        width: 500,
-                        gridRowGap: "20px",
-                        padding: "2em",
-                        margin: "2em",
-                    },
-                },
-            },
-            // MuiTypography: {
-            //     styleOverrides: {
-            //         root: {
-            //             color: "inherit",
-            //         },
-            //     },
-            // },
-            MuiButton: {
-                styleOverrides: {
-                    root: {
-                        color: "white",
-                        size: "large",
+                        display: "flex",
                     },
                 },
             },
