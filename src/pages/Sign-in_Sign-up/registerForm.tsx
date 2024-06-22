3f5087b63665230b9ffc38006a88458e1cf33de0
VadimLNN <svobodolubsev@inbox.ru>
2024-06-22 22:12:40 +0700

    подключение темы

--- src/pages/Sign-in_Sign-up/registerForm.tsx
+++ src/pages/Sign-in_Sign-up/registerForm.tsx
@@ -9,6 +9,8 @@ import DatePickerValue from "./ui/MuiDatePickerValue";
 import CityMuiBasicSelect from "./ui/CityMuiBasicSelect";
 import GenderMuiRadioGroup from "./ui/GenderMuiRadioGroup";
 import AgreeMuiCheckbox from "./ui/AgreeMuiCheckbox";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./ui/FormTheme";
 
 type FormValues = {
     name: string;
@@ -21,7 +23,7 @@ type FormValues = {
     agree: boolean;
 };
 
-export const MuiRegisterForm = () => {
+export const RegisterForm = () => {
     const validationSchema = yup.object({
         name: yup.string().required("Это поле обязательно"),
         surname: yup.string().required("Это поле обязательно"),
@@ -46,57 +48,59 @@ export const MuiRegisterForm = () => {
     const notify = () => toast("Заскамили мамонта");
 
     return (
-        <Card>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <Stack spacing={2} width={500}>
-                    <Typography variant="h4">Регистрация</Typography>
+        <ThemeProvider theme={theme}>
+            <Card>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <Stack spacing={2}>
+                        <Typography variant="h4">Регистрация</Typography>
 
-                    <TextField
-                        label="Имя"
-                        type="name"
-                        {...register("name", { required: "Это поле обязательно" })}
-                        error={!!errors.name}
-                        helperText={errors.name?.message}
-                    />
-                    <TextField
-                        label="Фамилия"
-                        type="surname"
-                        {...register("surname", { required: "Это поле обязательно" })}
-                        error={!!errors.surname}
-                        helperText={errors.surname?.message}
-                    />
+                        <TextField
+                            label="Имя"
+                            type="name"
+                            {...register("name", { required: "Это поле обязательно" })}
+                            error={!!errors.name}
+                            helperText={errors.name?.message}
+                        />
+                        <TextField
+                            label="Фамилия"
+                            type="surname"
+                            {...register("surname", { required: "Это поле обязательно" })}
+                            error={!!errors.surname}
+                            helperText={errors.surname?.message}
+                        />
 
-                    <DatePickerValue name={"dateOfBirth"} control={control} label={"Дата рождения"} errors={errors} />
+                        <DatePickerValue name={"dateOfBirth"} control={control} label={"Дата рождения"} errors={errors} />
 
-                    <GenderMuiRadioGroup name={"gender"} control={control} label={"Пол"} errors={errors} />
+                        <GenderMuiRadioGroup name={"gender"} control={control} label={"Пол"} errors={errors} />
 
-                    <CityMuiBasicSelect name={"city"} control={control} label={"Выберите ваш город"} errors={errors} />
+                        <CityMuiBasicSelect name={"city"} control={control} label={"Выберите ваш город"} errors={errors} />
 
-                    <TextField
-                        label="Почта"
-                        type="email"
-                        {...register("email", { required: "Email is requied" })}
-                        error={!!errors.email}
-                        helperText={errors.email?.message}
-                    />
-                    <TextField
-                        label="ПОРОЛЬ"
-                        type="password"
-                        {...register("password", { required: "Password is requied" })}
-                        error={!!errors.password}
-                        helperText={errors.password?.message}
-                    />
+                        <TextField
+                            label="Почта"
+                            type="email"
+                            {...register("email", { required: "Email is requied" })}
+                            error={!!errors.email}
+                            helperText={errors.email?.message}
+                        />
+                        <TextField
+                            label="ПОРОЛЬ"
+                            type="password"
+                            {...register("password", { required: "Password is requied" })}
+                            error={!!errors.password}
+                            helperText={errors.password?.message}
+                        />
 
-                    <AgreeMuiCheckbox name={"agree"} control={control} label={"Согласие на обработку персональных данных"} errors={errors} />
+                        <AgreeMuiCheckbox name={"agree"} control={control} label={"Согласие на обработку персональных данных"} errors={errors} />
 
-                    <Button type="submit" variant="contained" color="primary">
-                        Login
-                    </Button>
+                        <Button type="submit" variant="contained" color="primary">
+                            Login
+                        </Button>
 
-                    <ToastContainer />
-                </Stack>
-            </form>
-            <DevTool control={control} />
-        </Card>
+                        <ToastContainer />
+                    </Stack>
+                </form>
+                <DevTool control={control} />
+            </Card>
+        </ThemeProvider>
     );
 };
