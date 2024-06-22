4e907f4cc9750cb150e67d81519327469700c2bc
VadimLNN <svobodolubsev@inbox.ru>
2024-06-22 16:47:21 +0700

    удаление ненужного

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import { createPost, getImg, getPosts, getPostsController } from "../components/api/requests";
 import { Card } from "@mui/material";
 
