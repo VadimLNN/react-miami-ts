1705bb42e4af3404088d1cd2a2b786da0d927818
VadimLNN <svobodolubsev@inbox.ru>
2024-06-29 12:09:23 +0700

    доработки

--- src/pages/Main/ui/Skills.tsx
+++ src/pages/Main/ui/Skills.tsx
@@ -5,22 +5,21 @@ const Skills = () => {
     return (
         <Box
             sx={{
-                padding: "1em",
-                height: 500,
+                padding: "5vw",
                 marginTop: 3,
                 borderRadius: 4,
                 background: "radial-gradient(#3E7CFE, #2850A3)",
             }}
         >
-            <Typography sx={{ fontSize: "32px", marginTop: "1em", lineHeight: "1", color: "white" }}>Подтверждение Ваших навыков</Typography>
-            <CardMedia component="img" height="220" image={img_certificate} sx={{ borderRadius: 5, objectFit: "contain", marginTop: "1em" }} />
-            <Typography sx={{ fontSize: "14px", marginTop: "1em", lineHeight: "1.5", color: "white" }}>
+            <Typography sx={{ fontSize: "32px", marginTop: "5vw", lineHeight: "1.2", color: "white" }}>Подтверждение ваших навыков</Typography>
+            <CardMedia component="img" height="300vw" image={img_certificate} sx={{ borderRadius: 5, objectFit: "contain", marginTop: "4vw" }} />
+            <Typography sx={{ fontSize: "14px", marginBlock: "4vw", lineHeight: "1.5", color: "white" }}>
                 В конце обучения получите сертификат установленного образца. Мы обучаем по государственной лицензии №112ЛО57_56
             </Typography>
             <Button
                 fullWidth
                 variant="contained"
-                sx={{ width: "280px", marginTop: "1em", background: "linear-gradient(to right, #EE2F53, #992037)", borderRadius: 3 }}
+                sx={{ width: "78vw", marginBlock: "4vw", background: "linear-gradient(to right, #EE2F53, #992037)", borderRadius: 3 }}
             >
                 Посмотреть сертификат
             </Button>
