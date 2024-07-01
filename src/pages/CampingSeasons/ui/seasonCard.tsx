581054164d73e1bee6170e060dd991b1780d8226
VadimLNN <svobodolubsev@inbox.ru>
2024-07-01 15:52:43 +0700

    лютый компонент карточки смены

--- src/pages/CampingSeasons/ui/seasonCard.tsx
+++ src/pages/CampingSeasons/ui/seasonCard.tsx
@@ -1,25 +1,51 @@
-import { CardActionArea } from "@mui/material";
-import Card from "@mui/material/Card";
+import { Box, CardActionArea, CardMedia, Stack } from "@mui/material";
 import Typography from "@mui/material/Typography";
-// import CardMedia from "@mui/material/CardMedia";
 import CardContent from "@mui/material/CardContent";
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2";
 
-const CampSeason = () => {
-    return (
-        <Card sx={{ maxWidth: 345 }}>
-            <CardActionArea>
-                {/* <CardMedia component="img" height="140" image="/static/images/cards/contemplative-reptile.jpg" alt="green iguana" /> */}
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        Lizard
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all continents except Antarctica
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
-    );
-};
+interface SeasonProps {
+    title: string;
+    text: string;
+    tegs: string[];
+    img: string;
+    color: string;
+}
+
+const CampSeason = (props: SeasonProps): JSX.Element => (
+    <Box sx={{ borderRadius: 5, background: `${props.color}`, margin: "5vw" }}>
+        <CardActionArea>
+            <Grid2 container sx={{ padding: "2vw" }}>
+                <Grid2 xs={6}>
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {props.title}
+                        </Typography>
+                        <Typography variant="body2" color="fw-bold">
+                            {props.text}
+                        </Typography>
+                    </CardContent>
+                </Grid2>
+                <Grid2 xs={6}>
+                    <CardMedia sx={{ margin: "auto", marginTop: "4vw" }} component="img" image={`/src/shared/ui/imgs/${props.img}`} />
+                </Grid2>
+                <Stack direction={"row"} useFlexGap flexWrap="wrap">
+                    {props.tegs.map((item) => (
+                        <Typography
+                            sx={{
+                                fontSize: "10pt",
+                                border: "1px hsla(210, 65%, 12%, 1) solid",
+                                borderRadius: "10px",
+                                paddingInline: "4.5vw",
+                                margin: "1vw",
+                            }}
+                        >
+                            {item}
+                        </Typography>
+                    ))}
+                </Stack>
+            </Grid2>
+        </CardActionArea>
+    </Box>
+);
 
 export default CampSeason;
