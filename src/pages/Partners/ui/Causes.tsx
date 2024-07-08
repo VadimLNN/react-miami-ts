de55a5446eb9ff63a888b657c4ccc7add60c74f1
алевтина19 <potekhina_04_04@mail.ru>
2024-07-08 11:44:18 +0700

    добавление отступов

--- src/pages/Partners/ui/Causes.tsx
+++ src/pages/Partners/ui/Causes.tsx
@@ -9,10 +9,10 @@ const Causes = (props: CausesProps): JSX.Element => (
     <>
         <Box sx={{ minHeight: "80px", bgcolor: alpha("#FAE4FD", 0.05), color: "white", margin: "3vh", borderRadius: 4 }}>
             <Stack direction={"row"}>
-                <Typography variant="h4" sx={{ opacity: 1 }}>
+                <Typography variant="h4" sx={{ opacity: 1, margin: "6%" }}>
                     {props.num}
                 </Typography>
-                <Typography sx={{ opacity: 1 }}>{props.text} </Typography>
+                <Typography sx={{ opacity: 1, margin: "5% 2%" }}>{props.text} </Typography>
             </Stack>
         </Box>
     </>
